1e613048d37d976efb8b7560904bb2d262ba5bc5
tintoll <belselios@gmail.com>
2020-11-07 23:11:15 +0900

    init

---
+++ android/app/src/main/kotlin/io/tintoll/instagram_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.tintoll.instagram_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart'; // 구글 디자인 레이아웃
+// import 'package:flutter/cupertino.dart'; // IOS 디자인
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
