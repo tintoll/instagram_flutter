932dc6a028471a98b161cb034e900dbe03c56b5c
tintoll <belselios@gmail.com>
2020-11-08 22:36:32 +0900

    bottom navbar add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart'; // 구글 디자인 레이아웃
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/home_page.dart'; // 구글 디자인 레이아웃
 // import 'package:flutter/cupertino.dart'; // IOS 디자인
 
 void main() {
@@ -9,12 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Scaffold(
-        appBar: AppBar(
-          title : Text('Cool App')
-        ),
-        body: Container(color: Colors.amber,),
-      )
+      home : HomePage()
     );
   }
 }
+
+
