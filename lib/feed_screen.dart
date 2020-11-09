81de7f2b554105970ce9df79a721ba6f53f0782e
tintoll <belselios@gmail.com>
2020-11-10 06:00:27 +0900

    change Appbar to CupertinoNavigationBar & Use Asset Image

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,19 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon : Icon(
+            Icons.camera_alt,
+            color: Colors.black87,
+          )
+        ),
+        middle: Text(
             'instagram',
             style: TextStyle(
               fontFamily: 'VeganStyle'
             ),
           ),
+          trailing : Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/actionbar_camera.png'),
+                    color: Colors.black87,
+                  ),
+                  onPressed: null
+              ),
+              IconButton(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/actionbar_camera.png'),
+                    color: Colors.black87,
+                  ),
+                  onPressed: null
+              ),
+            ],
+          )
         ),
-      ),
+        
       body: ListView.builder(itemBuilder: (BuildContext context, int index){
         return Container(
           color: Colors.accents[index % Colors.accents.length],
