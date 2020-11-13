f23a5723968ddb292cc05cfdeb5d532085df1f98
tintoll <belselios@gmail.com>
2020-11-13 09:43:00 +0900

    create post header and actions and likes

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/constants/common_size.dart';
 import 'package:instagram_flutter/widgets/my_progress_indicator.dart';
+import 'package:instagram_flutter/widgets/rounded_avatar.dart';
 
 class Post extends StatelessWidget {
   final int index;
@@ -15,21 +17,84 @@ class Post extends StatelessWidget {
   Widget build(BuildContext context) {
     if (size == null) size = MediaQuery.of(context).size;
 
-    return CachedNetworkImage(
-      placeholder: (BuildContext context, String url) {
-        return MyProgressIndicator(containerSize: size.width,);
-      },
-      imageUrl: 'https://picsum.photos/id/$index/1000/1000',
-      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
-        return AspectRatio(
-          aspectRatio: 1,
-          child: Container(
-            decoration: BoxDecoration(
-                image:
-                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
-          ),
-        );
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _postHeader(),
+        _postImage(),
+        _postActions(),
+        _postLikes(),
+      ],
     );
   }
+
+  Padding _postLikes() {
+    return Padding(
+        padding: const EdgeInsets.all(common_gap),
+        child: Text('12000 likes', style: TextStyle(fontWeight: FontWeight.bold),)
+    );
+  }
+
+  Row _postActions() {
+    return Row(children: [
+      IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/images/bookmark.png'),
+            color: Colors.black87,),
+          onPressed: null
+      ),
+      IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/images/comment.png'),
+            color: Colors.black87,),
+          onPressed: null
+      ),
+      IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/images/direct_message.png'),
+            color: Colors.black87,),
+          onPressed: null
+      ),
+      Spacer(),
+      IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/images/heart_selected.png'),
+            color: Colors.black87,),
+          onPressed: null
+      ),
+    ],);
+  }
+
+  Row _postHeader() {
+    return Row(children: [
+      Padding(
+        padding: const EdgeInsets.all(common_xxs_gap),
+        child: RoundedAvatar()
+      ),
+      Expanded(child: Text('username')),
+      IconButton(icon: Icon(Icons.more_vert, color: Colors.black87,), onPressed: null)
+    ],);
+  }
+  CachedNetworkImage _postImage() {
+    return CachedNetworkImage(
+    placeholder: (BuildContext context, String url) {
+      return MyProgressIndicator(containerSize: size.width,);
+    },
+    imageUrl: 'https://picsum.photos/id/$index/1000/1000',
+    imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+      return AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+        ),
+      );
+    },
+  );
+  }
+
+
 }
+
+
