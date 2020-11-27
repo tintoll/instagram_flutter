14fb7853bd0f31ecc0adeca3e62564517027d5ff
tintoll <belselios@gmail.com>
2020-11-28 07:47:20 +0900

    make camera screen layout

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -6,8 +6,52 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.cyanAccent,);
+    return Scaffold(
+      body: PageView(
+        children: [
+          Container(
+            color: Colors.cyanAccent,
+          ),
+          Container(
+            color: Colors.redAccent,
+          ),
+          Container(
+            color: Colors.greenAccent,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 0,
+        selectedItemColor: Colors.black,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        unselectedItemColor: Colors.black54,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.radio_button_checked),
+            label: "GALLAY",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.radio_button_checked),
+            label: "PHOTO",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.radio_button_checked),
+            label: "VIDEO",
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: _onItemTabbed,
+      ),
+    );
+  }
+
+  void _onItemTabbed(index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
