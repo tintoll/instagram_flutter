d48d516dd4923ef2e0f2fc96efd09e8790d0d3a9
tintoll <belselios@gmail.com>
2020-11-29 06:14:04 +0900

    make pageview to move with tabbar and scollview

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -7,11 +7,19 @@ class CameraScreen extends StatefulWidget {
 
 class _CameraScreenState extends State<CameraScreen> {
   int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: _pageController,
         children: [
           Container(
             color: Colors.cyanAccent,
@@ -23,6 +31,11 @@ class _CameraScreenState extends State<CameraScreen> {
             color: Colors.greenAccent,
           ),
         ],
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         iconSize: 0,
@@ -52,6 +65,8 @@ class _CameraScreenState extends State<CameraScreen> {
   void _onItemTabbed(index) {
     setState(() {
       _currentIndex = index;
+      _pageController.animateToPage(_currentIndex,
+          duration: Duration(milliseconds: 200), curve: Curves.fastOutSlowIn);
     });
   }
 }
