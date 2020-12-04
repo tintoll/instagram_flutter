cbaa610cc5a32e75a3558c6875320557c0843944
tintoll <belselios@gmail.com>
2020-12-05 05:51:36 +0900

    make camera preview

--- lib/widgets/take_photo.dart
+++ lib/widgets/take_photo.dart
@@ -1,35 +1,61 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/constants/common_size.dart';
 import 'package:instagram_flutter/constants/screen_size.dart';
+import 'package:instagram_flutter/widgets/my_progress_indicator.dart';
 
-class TakePhoto extends StatelessWidget {
+class TakePhoto extends StatefulWidget {
   const TakePhoto({
     Key key,
   }) : super(key: key);
 
   @override
+  _TakePhotoState createState() => _TakePhotoState();
+}
+
+class _TakePhotoState extends State<TakePhoto> {
+  CameraController _controller;
+  Widget _progress = MyProgressIndicator();
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: size.width,
-          height: size.width,
-          color: Colors.black,
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(common_gap),
-            child: OutlineButton(
-              shape: CircleBorder(),
-              onPressed: () {},
-              borderSide: BorderSide(
-                width: 20,
-                color: Colors.black12
+    return FutureBuilder<List<CameraDescription>>(
+        future: availableCameras(),
+        builder: (context, snapshot) {
+          return Column(
+            children: [
+              Container(
+                width: size.width,
+                height: size.width,
+                color: Colors.black,
+                child: (snapshot.hasData) ? _getPreview(snapshot.data) : _progress,
               ),
-            ),
-          ),
-        ),
-      ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(common_gap),
+                  child: OutlineButton(
+                    shape: CircleBorder(),
+                    onPressed: () {},
+                    borderSide: BorderSide(width: 20, color: Colors.black12),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Widget _getPreview(cameras) {
+    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    return FutureBuilder<void>(
+      future: _controller.initialize(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return CameraPreview(_controller);
+        } else {
+          return _progress;
+        }
+      },
     );
   }
 }
