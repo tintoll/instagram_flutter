e5d007ed5549b7e0a1c94631a508504e8d350f38
tintoll <belselios@gmail.com>
2020-12-09 06:55:14 +0900

    use multi provider

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/camera_state.dart';
 import 'package:instagram_flutter/widgets/take_photo.dart';
+import 'package:provider/provider.dart';
 
 class CameraScreen extends StatefulWidget {
+
+  CameraState _cameraState = CameraState();
+
   @override
-  _CameraScreenState createState() => _CameraScreenState();
+  _CameraScreenState createState() {
+    _cameraState.getReadyToTakePhoto();
+    return _CameraScreenState();
+  }
+
 }
 
 class _CameraScreenState extends State<CameraScreen> {
@@ -19,59 +28,64 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-      ),
-      body: PageView(
-        controller: _pageController,
-        children: [
-          Container(
-            color: Colors.cyanAccent,
-          ),
-          TakePhoto(),
-          Container(
-            color: Colors.greenAccent,
-          ),
-        ],
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-            switch (index) {
-              case 0:
-                _title = "Gallery";
-                break;
-              case 1:
-                _title = "Photo";
-                break;
-              case 2:
-                _title = "Video";
-                break;
-            }
-          });
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 0,
-        selectedItemColor: Colors.black,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        unselectedItemColor: Colors.black54,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.radio_button_checked),
-            label: "GALLERY",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.radio_button_checked),
-            label: "PHOTO",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.radio_button_checked),
-            label: "VIDEO",
-          ),
-        ],
-        currentIndex: _currentIndex,
-        onTap: _onItemTabbed,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: widget._cameraState)
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_title),
+        ),
+        body: PageView(
+          controller: _pageController,
+          children: [
+            Container(
+              color: Colors.cyanAccent,
+            ),
+            TakePhoto(),
+            Container(
+              color: Colors.greenAccent,
+            ),
+          ],
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+              switch (index) {
+                case 0:
+                  _title = "Gallery";
+                  break;
+                case 1:
+                  _title = "Photo";
+                  break;
+                case 2:
+                  _title = "Video";
+                  break;
+              }
+            });
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          iconSize: 0,
+          selectedItemColor: Colors.black,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          unselectedItemColor: Colors.black54,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked),
+              label: "GALLERY",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked),
+              label: "PHOTO",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked),
+              label: "VIDEO",
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: _onItemTabbed,
+        ),
       ),
     );
   }
