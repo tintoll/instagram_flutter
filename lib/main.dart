332e4f090c9e8f1d2579160957264823afc8dd06
tintoll <belselios@gmail.com>
2020-12-20 06:11:42 +0900

    inject FirebaseAuthState via Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/constants/material_white.dart';
 import 'package:instagram_flutter/home_page.dart';
+import 'package:instagram_flutter/models/firebase_auth_state.dart';
+import 'package:instagram_flutter/screens/auth_screen.dart';
+import 'package:instagram_flutter/widgets/my_progress_indicator.dart';
+import 'package:provider/provider.dart';
 // import 'package:flutter/cupertino.dart'; // IOS 디자인
 
 void main() {
@@ -8,11 +12,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  FirebaseAuthState _firebaseAuthState = FirebaseAuthState();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: white),
+    return ChangeNotifierProvider.value(
+      value: _firebaseAuthState,
+      child: MaterialApp(
+        home: Consumer(
+            builder: (BuildContext context, FirebaseAuthState firebaseAuthState,
+                Widget child) {
+              switch (firebaseAuthState.firebaseAuthStatus) {
+                case FirebaseAuthStatus.signout:
+                  return AuthScreen();
+                case FirebaseAuthStatus.progress:
+                  return MyProgressIndicator();
+                case FirebaseAuthStatus.signin:
+                  return HomePage();
+                default:
+                  return MyProgressIndicator();
+              }
+            },
+        ),
+        theme: ThemeData(primarySwatch: white),
+      ),
     );
   }
 }
--- lib/models/firebase_auth_state.dart
+++ lib/models/firebase_auth_state.dart
@@ -2,15 +2,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
 class FirebaseAuthState extends ChangeNotifier {
-
   FirebaseAuthStatus _firebaseAuthStatus = FirebaseAuthStatus.progress;
   User _firebaseUser;
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void watchAuthChange() {
+    _firebaseAuth.authStateChanges().listen((user) {
+      if (user == null && _firebaseUser == null) {
+        return;
+      } else if(user != _firebaseUser){
+        _firebaseUser = user;
+        changeFirebaseStatus();
+      }
+    });
+  }
 
   void changeFirebaseStatus([FirebaseAuthStatus firebaseAuthStatus]) {
-    if(firebaseAuthStatus != null) {
+    if (firebaseAuthStatus != null) {
       _firebaseAuthStatus = firebaseAuthStatus;
     } else {
-      if(_firebaseUser != null) {
+      if (_firebaseUser != null) {
         _firebaseAuthStatus = FirebaseAuthStatus.signin;
       } else {
         _firebaseAuthStatus = FirebaseAuthStatus.signout;
@@ -19,8 +30,7 @@ class FirebaseAuthState extends ChangeNotifier {
     notifyListeners();
   }
 
+  FirebaseAuthStatus get firebaseAuthStatus => _firebaseAuthStatus;
 }
 
-enum FirebaseAuthStatus {
-  signout, progress, signin
-}
+enum FirebaseAuthStatus { signout, progress, signin }
