84b6825af7b9611ab02750147868ecd70d89916d
tintoll <belselios@gmail.com>
2020-12-21 06:12:35 +0900

    change flutter_auth version to 0.18 and set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/constants/material_white.dart';
 import 'package:instagram_flutter/home_page.dart';
@@ -7,7 +8,9 @@ import 'package:instagram_flutter/widgets/my_progress_indicator.dart';
 import 'package:provider/provider.dart';
 // import 'package:flutter/cupertino.dart'; // IOS 디자인
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -16,10 +19,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
+    return ChangeNotifierProvider<FirebaseAuthState>.value(
       value: _firebaseAuthState,
       child: MaterialApp(
-        home: Consumer(
+        home: Consumer<FirebaseAuthState>(
           builder: (BuildContext context, FirebaseAuthState firebaseAuthState,
               Widget child) {
             switch (firebaseAuthState.firebaseAuthStatus) {
