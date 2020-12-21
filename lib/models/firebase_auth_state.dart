bc955d923783b5f371f927baefe687f4972e4772
tintoll <belselios@gmail.com>
2020-12-22 05:31:38 +0900

    handle on error for login

--- lib/models/firebase_auth_state.dart
+++ lib/models/firebase_auth_state.dart
@@ -21,7 +21,6 @@ class FirebaseAuthState extends ChangeNotifier {
   void registerUser(BuildContext context, {@required String email, @required String password}) {
     _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)
         .catchError((error) {
-         print(error.code);
          String _message = "";
          switch(error.code) {
            case "email-already-in-use" :
@@ -38,11 +37,32 @@ class FirebaseAuthState extends ChangeNotifier {
              break;
          }
          SnackBar snackBar = SnackBar(content: Text(_message));
+         // Scaffold 아래 있는 context를 가져와야된다.
          return Scaffold.of(context).showSnackBar(snackBar);
     });
   }
-  void login({@required String email, @required String password}) {
-    _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  void login(BuildContext context, {@required String email, @required String password}) {
+    _firebaseAuth.signInWithEmailAndPassword(email: email.trim(), password: password.trim())
+        .catchError((error) {
+        String _message = "";
+        switch(error.code) {
+          case "invalid-email" :
+            _message = "잘못된 이메일주소인데";
+            break;
+          case "user-disabled" :
+            _message = "해당사용자 금지";
+            break;
+          case "user-not-found" :
+            _message = "사용자가 없는데";
+            break;
+          case "wrong-password" :
+            _message = "잘못된 비밀번호인데";
+            break;
+        }
+        SnackBar snackBar = SnackBar(content: Text(_message));
+        // Scaffold 아래 있는 context를 가져와야된다.
+        return Scaffold.of(context).showSnackBar(snackBar);
+    });
   }
   void signOut() {
     _firebaseAuthStatus = FirebaseAuthStatus.signout;
--- lib/widgets/sing_in_form.dart
+++ lib/widgets/sing_in_form.dart
@@ -104,7 +104,7 @@ class _SignInFormState extends State<SignInForm> {
       onPressed: () {
         if (_formKey.currentState.validate()) {
           Provider.of<FirebaseAuthState>(context, listen: false)
-              .login(email: _emailController.text, password: _pwController.text);
+              .login(context, email: _emailController.text, password: _pwController.text);
         }
       },
       color: Colors.blue,
