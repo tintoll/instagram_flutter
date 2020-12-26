dddbe8bc1dec869514fc4449c046f7e5afa85e7d
tintoll <belselios@gmail.com>
2020-12-27 06:01:30 +0900

    create firestore UserModel

---
+++ lib/models/firestore/user_model.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_flutter/constants/firestore_keys.dart';
+
+class UserModel {
+  final String userKey;
+  final String profileImg;
+  final String email;
+  final List<dynamic> myPosts;
+  final int followers;
+  final List<dynamic> likedPosts;
+  final String username;
+  final List<dynamic> followings;
+  final DocumentReference reference;
+
+  UserModel.fromMap(Map<String, dynamic> map, this.userKey, {this.reference})
+      : profileImg = map[KEY_PROFILEIMG],
+        username = map[KEY_USERNMAE],
+        email = map[KEY_EMAIL],
+        likedPosts = map[KEY_LIKEDPOSTS],
+        followers = map[KEY_FOLLOWERS],
+        followings = map[KEY_FOLLOWINGS],
+        myPosts = map[KEY_MYPOSTS];
+
+  UserModel.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), snapshot.id, reference : snapshot.reference);
+}
