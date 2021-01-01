18b70d10c174306e1e560ea2ea43ff3cdb0d4ce1
tintoll <belselios@gmail.com>
2021-01-02 06:24:51 +0900

    add section button layout in share post

--- lib/screens/share_post_screen.dart
+++ lib/screens/share_post_screen.dart
@@ -31,21 +31,50 @@ class SharePostScreen extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                  vertical: common_gap, horizontal: common_gap),
-              leading: Image.file(
-                imageFile,
-                width: size.width / 6,
-                height: size.width / 6,
-                fit: BoxFit.cover,
-              ),
-              title: TextField(
-                decoration: InputDecoration(
-                    hintText: "Write a caption...", border: InputBorder.none),
-              ),
-            ),
+            _captionWithImage(),
+            _divider,
+            _sectionButton('Tag People'),
+            _divider,
+            _sectionButton('Add Location'),
           ],
         ));
+  }
+
+  ListTile _sectionButton(String title) {
+    return ListTile(
+            title: Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            trailing: Icon(
+              Icons.navigate_next,
+              color: Colors.grey[400],
+            ),
+            dense: true,
+            contentPadding: EdgeInsets.symmetric(horizontal: common_gap),
+          );
+  }
+
+  Divider get _divider => Divider(
+        color: Colors.grey[300],
+        height: 1,
+        thickness: 1,
+      );
+
+  ListTile _captionWithImage() {
+    return ListTile(
+      contentPadding:
+          EdgeInsets.symmetric(vertical: common_gap, horizontal: common_gap),
+      leading: Image.file(
+        imageFile,
+        width: size.width / 6,
+        height: size.width / 6,
+        fit: BoxFit.cover,
+      ),
+      title: TextField(
+        decoration: InputDecoration(
+            hintText: "Write a caption...", border: InputBorder.none),
+      ),
+    );
   }
 }
