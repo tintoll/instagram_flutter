2bc2d803d799d5f83247dc64c15273b53dff3428
tintoll <belselios@gmail.com>
2021-01-07 05:17:52 +0900

    create Post Model

--- lib/constants/firestore_keys.dart
+++ lib/constants/firestore_keys.dart
@@ -14,12 +14,13 @@ const KEY_USERNMAE = 'username';
 const KEY_COMMENT = 'comment';
 const KEY_COMMENTTIME = 'comment_time';
 
-const KEY_POSTIMG = 'postimg';
-const KEY_POSTURI = 'posturi';
+const KEY_POSTKEY = 'post_key';
+const KEY_POSTIMG = 'post_img';
+const KEY_POSTURI = 'post_uri';
 const KEY_CAPTION = 'caption';
 const KEY_LASTCOMMENT = 'last_comment';
 const KEY_LASTCOMMENTOR = 'last_commentor';
-const KEY_LASTCOMMENTTIME = 'last_commenttime';
+const KEY_LASTCOMMENTTIME = 'last_comment_time';
 const KEY_NUMOFLIKES = 'num_of_likes';
 const KEY_NUMOFCOMMENTS = 'num_of_comments';
 const KEY_POSTTIME = 'post_time';
