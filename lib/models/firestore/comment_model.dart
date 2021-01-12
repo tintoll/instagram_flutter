337fa2df33e09cde67375feada2fede0bb83d094
tintoll <belselios@gmail.com>
2021-01-13 05:11:52 +0900

    create Comment Model

---
+++ lib/models/firestore/comment_model.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_flutter/constants/firestore_keys.dart';
+
+class CommentModel {
+  final String username;
+  final String userKey;
+  final String comment;
+  final DateTime commentTime;
+  final String commentKey;
+  final DocumentReference reference;
+
+  CommentModel.fromMap(Map<String, dynamic> map, this.commentKey, {this.reference})
+      : username = map[KEY_USERNMAE],
+        userKey = map[KEY_USERKEY],
+        comment = map[KEY_COMMENT],
+        commentTime = map[KEY_COMMENTTIME] == null ? DateTime.now().toUtc(): (map[KEY_COMMENTTIME] as Timestamp).toDate();
+
+  CommentModel.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), snapshot.id,
+      reference: snapshot.reference);
+
+  static Map<String, dynamic> getMapForNewComment(String userKey, String username, String comment) {
+    Map<String, dynamic> map = Map();
+    map[KEY_USERKEY] = userKey;
+    map[KEY_USERNMAE] = username;
+    map[KEY_COMMENT] = comment;
+    map[KEY_COMMENTTIME] = DateTime.now().toUtc();
+    return map;
+  }
+
+}
